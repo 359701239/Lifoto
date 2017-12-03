232d17371229280308e74f16eed7021964df6458
左杰 <359701239@qq.com>
2017-12-03 23:42:01 +0800

    UI搭建

--- app/src/main/java/com/external/lifoto/MainActivity.java
+++ app/src/main/java/com/external/lifoto/MainActivity.java
@@ -1,16 +1,28 @@
 package com.external.lifoto;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.external.lifoto.adapter.MainPagerAdapter;
-import com.external.lifoto.fragment.SortFragment;
 import com.external.lifoto.content.TabList;
 import com.external.lifoto.design.widget.FitToolbar;
+import com.external.lifoto.fragment.SortFragment;
 
 import java.util.ArrayList;
 
@@ -20,24 +32,64 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
     private FitToolbar toolbar;
     private AppBarLayout appBarLayout;
     private TabLayout tabLayout;
     private ViewPager pager;
+    private TextView title;
 
     private MainPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_main);
 
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        navigationView = (NavigationView) findViewById(R.id.navigation);
         toolbar = (FitToolbar) findViewById(R.id.toolbar);
         appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
         tabLayout = (TabLayout) findViewById(R.id.tab);
         pager = (ViewPager) findViewById(R.id.pager);
+        title = (TextView) findViewById(R.id.title);
 
+        init();
         initPager();
+    }
+
+    private void init() {
+        title.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/ordinary.ttf"));
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.setting:
+                        Toast.makeText(MainActivity.this,
+                                "sub item 01", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                drawer.closeDrawers();
+                return true;
+            }
+        });
     }
 
     private void initPager() {
